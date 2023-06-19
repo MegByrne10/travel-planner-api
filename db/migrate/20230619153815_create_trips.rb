36ec13b3e69df5c19f00d1975647b461b0f23241
Megan Byrne <megsgetsemail@gmail.com>
2023-06-19 13:22:42 -0400

    Create trips model/endpoints and add base react app

---
+++ db/migrate/20230619153815_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trips do |t|
+      t.string :title
+      t.text :description
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/trips.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :trip do
+    title { "MyString" }
+    description { "MyText" }
+    start_date { "2023-06-19" }
+    end_date { "2023-06-19" }
+  end
+end
